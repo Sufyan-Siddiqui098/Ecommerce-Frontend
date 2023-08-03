35d15c872a798151a8c8000ca9f7c0a582291c17
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-08-03 18:53:07 +0500

    some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+  
   return (
     <header>
-        header
+          <Link to='/' className='link logo'>Brands</Link>
+        <nav>
+            <Link className='link' to='/'>Home</Link>
+            <Link className='link' to='/category'>Category</Link>
+            <Link className='link' to='/register'>Register</Link>
+            <Link className='link' to='/login'>Login</Link>
+            <Link className='link' to='/cart'>Cart(0)</Link>
+
+        </nav>
     </header>
   )
 }
