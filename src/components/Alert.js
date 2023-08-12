1475a61889dd343127200b6ca22cc6cb8ee5463a
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-08-13 04:11:00 +0500

    alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Alert = () => {
+  const msg = useSelector((state)=>state.user.message);
+  const err = useSelector((state) => state.error)
   return (
-    <div>Alert</div>
+    <div className={`msg ${err?"error":"success"}`}>
+      <p>{msg}</p>
+    </div>
   )
 }
 
