23c179ab178e32e35dced001660ba06afcf6b9c2
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-08-26 22:44:12 +0500

    alert is modified with done/reject icon

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import './alert.css'
+import {MdOutlineDownloadDone} from 'react-icons/md'
+import {RxCross2} from 'react-icons/rx'
 
 const Alert = () => {
   const {message, error, alert} = useSelector((state)=>state.user);
   
   return (
     <div className={`alert ${alert? "show":"hide"} ${error?"error":"success"}`}>
-      <p>{message}</p>
+      <p className='flex'>{error? <RxCross2 className='icon' /> : <MdOutlineDownloadDone className='icon'/>}{`${message}`}</p>
     </div>
   )
 }
