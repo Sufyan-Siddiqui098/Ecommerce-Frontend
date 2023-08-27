9e08e0b4125731ee80f7879141e10a5da92664a9
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-08-28 01:43:52 +0500

    login handler inside login componenet to conveniently deliver better

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,43 +1,79 @@
-import React,{useState} from 'react';
-import '../styles/register.css'
-import { loginUser, switchAlert } from '../../store/UserSlice';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import "../styles/register.css";
+import { loginUser, switchAlert } from "../../store/UserSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const dispatch = useDispatch()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const user = {email, password}
-         dispatch(loginUser(user))
-        //  navigate("/login")
-         setTimeout(() => {
-          dispatch(switchAlert())
-         }, 2500);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = { email, password };
+      const response = await fetch(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-TYpe": "application/json",
+          },
+          body: JSON.stringify(user),
+        }
+      );
+
+      const json = await response.json();
+      dispatch(loginUser(json));
+
+      if (json.success) {
+        navigate("/");
       }
+      // -------- HIDE ALERT MESSAGE
+      setTimeout(() => {
+        dispatch(switchAlert());
+      }, 2000);
+    } catch (error) {
+      console.log("error in login", error);
+    }
+  };
 
   return (
-    <div className='register'>
-    <h1>Login Page</h1>
-    <form onSubmit={handleSubmit}>
-        
-      <div className="field">
-        <label htmlFor="emial">Email</label>
-        <input type="email" name="email" id="email" placeholder='example@gmail.com' required value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      </div>
-      <div className="field">
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" placeholder='Type password here' required value={password} onChange={(e)=>setPassword(e.target.value)} />
-      </div>
-      
-      
-      <button className='btn-submit'>Login</button>
-    </form>
-</div>
-  )
-}
+    <div className="register">
+      <h1>Login Page</h1>
+      <form onSubmit={handleLogin}>
+        <div className="field">
+          <label htmlFor="emial">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="example@gmail.com"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Type password here"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-export default Login
+        <button className="btn-submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
