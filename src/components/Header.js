95d91de69114dd5f9da8b67433c4319645506725
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-09-07 23:57:51 +0500

    Logout button and function created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,76 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom'
-import {RxHamburgerMenu} from 'react-icons/rx'
-import Alert from './Alert'
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
+import Alert from "./Alert";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser, switchAlert } from "../store/UserSlice";
 
 const Header = () => {
-  const navBar = useRef(null)
-  const sideBar = useRef(null)
-  const activeNavBar = ()=>{
-    if(navBar.current.classList.contains('active')){
-      navBar.current.classList.remove('active')
-      sideBar.current.style.display= 'none'
+  const navBar = useRef(null);
+  const sideBar = useRef(null);
+  const dispatch = useDispatch();
+
+  const { authToken } = useSelector((state) => state.user);
+  //-- For Hamburger Icon
+  const activeNavBar = () => {
+    if (navBar.current.classList.contains("active")) {
+      navBar.current.classList.remove("active");
+      sideBar.current.style.display = "none";
+    } else {
+      sideBar.current.style.display = "unset";
+      navBar.current.classList.add("active");
     }
-    else{
-      sideBar.current.style.display = 'unset'
-      navBar.current.classList.add('active')
+  };
+  //-- Deactive the side bar
+  const deActiveNavBar = (e) => {
+    if (!e.target.classList.contains("active")) {
+      sideBar.current.style.display = "none";
+      navBar.current.classList.remove("active");
     }
+  };
+
+  //Logout User
+  const logout = ()=> {
+    dispatch(logoutUser("Logout Successfully"))
+    setTimeout(() => {
+      dispatch(switchAlert());
+    }, 2000);
   }
-  
-  const deActiveNavBar = (e)=>{
-    if(!e.target.classList.contains("active")){
-      sideBar.current.style.display= 'none'
-      navBar.current.classList.remove("active")
-    }
-  }
-  
+
   return (
     <>
-    <header>
-          <Link to='/' className='link logo'>🛒 Ecommerce App</Link>
-          <div className="sideBar" ref={sideBar} onClick={deActiveNavBar}>
-          </div>
-            <RxHamburgerMenu className='menu' onClick={activeNavBar}/>
+      <header>
+        <Link to="/" className="link logo">
+          🛒 Ecommerce App
+        </Link>
+        <div className="sideBar" ref={sideBar} onClick={deActiveNavBar}></div>
+        <RxHamburgerMenu className="menu" onClick={activeNavBar} />
         <nav ref={navBar}>
-            <Link className='link nav-link' onClick={deActiveNavBar} to='/'>Home</Link>
-            <Link className='link nav-link' onClick={deActiveNavBar} to='/category'>Category</Link>
-            <Link className='link nav-link' onClick={deActiveNavBar} to='/register'>Register</Link>
-            <Link className='link nav-link' onClick={deActiveNavBar} to='/login'>Login</Link>
-            <Link className='link nav-link' onClick={deActiveNavBar} to='/cart'>Cart (0)</Link>
-
+          <Link className="link nav-link" onClick={deActiveNavBar} to="/">
+            Home
+          </Link>
+          <Link
+            className="link nav-link"
+            onClick={deActiveNavBar}
+            to="/category"
+          >
+            Category
+          </Link>
+          {authToken ? 
+              (<>
+              <Link className="link nav-link" onClick={deActiveNavBar} to="/cart">Cart (0)</Link>
+              <Link className="link btn"onClick={logout} to="/login">Logout</Link>
+              </>) : (<>
+              <Link className="link nav-link" onClick={deActiveNavBar} to="/register">Register</Link>
+              <Link className="link nav-link" onClick={deActiveNavBar} to="/login">Login</Link>
+              <Link className="link nav-link" onClick={deActiveNavBar} to="/cart">Cart (0)</Link>
+                    </>)
+          }
         </nav>
-    <Alert/>
-    </header>
+        <Alert />
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
