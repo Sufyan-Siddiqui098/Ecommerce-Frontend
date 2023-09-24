cf9a83a793f86c1add2c93f00abd8f192ea619b6
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-09-25 02:41:56 +0500

    user dashboard

--- src/pages/user/Dashboard.js
+++ src/pages/user/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-        <h1 className='text-4xl text-center'>Dashboard</h1>
+        <h1 className='text-4xl text-center my-3'> User Dashboard</h1>
     </div>
   )
 }
