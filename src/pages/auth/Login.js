598cef05711a30bb86195d787510295e4ae8ee14
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-10-05 16:27:58 +0500

    typo in handleLogin fix

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
         {
           method: "POST",
           headers: {
-            "Content-TYpe": "application/json",
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(user),
         }
