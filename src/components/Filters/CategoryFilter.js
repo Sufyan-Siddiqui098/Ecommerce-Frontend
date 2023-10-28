5655eee45dcbc0d14ca8c796e45254e83e26245a
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-10-28 15:40:45 +0500

    filters functionality working with price, and category

---
+++ src/components/Filters/CategoryFilter.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const CategoryFilter = ({
+  categories,
+  setMenu,
+  menu,
+  handleFilterCategory,
+}) => {
+  return (
+    <>
+      <div
+        className={`text-sm sm:text-base ${
+          menu ? "flex w-[50vw] visible" : "p-0 w-0 overflow-hidden invisible"
+        } px-1 py-3 min-h-min flex-col items-center border rounded-lg bg-gray-100 transition-[width] my-7 sm:flex sm:w-64 sm:px-4 sm:pb-3 sm:visible `}
+      >
+        <h5 className="self-start mb-3 text-gray-500 font-semibold">
+          By Category
+        </h5>
+        {categories?.map((category) => (
+          <fieldset
+            key={category._id}
+            className="flex gap-1  w-full border-b-2 border-gray-400"
+          >
+            <input
+              onChange={(e) =>
+                handleFilterCategory(e.target.checked, category._id)
+              }
+              type="checkbox"
+              name={`${category.slug}`}
+              id={`${category.slug}`}
+            />
+            <label className="w-full " htmlFor={`${category.slug}`}>
+              {category.name}
+            </label>
+          </fieldset>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CategoryFilter;
---
+++ src/components/Filters/PriceFilter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Prices } from "./Prices";
+
+const PriceFilter = ({ menu, setRadio }) => {
+  return (
+    <div
+      className={`text-sm sm:text-base ${
+        menu ? "flex w-[50vw] visible" : "p-0 w-0 overflow-hidden invisible"
+      } px-1 py-3 min-h-min flex-col items-center border rounded-lg bg-gray-100 transition-[width] my-7 sm:flex sm:w-64 sm:px-4 sm:pb-3 sm:visible `}
+    >
+      <h5 className="self-start mb-3 text-gray-500 font-semibold">By Price</h5>
+      {Prices?.map((p) => (
+        <fieldset
+          key={p._id}
+          className="flex gap-1  w-full border-b-2 border-gray-400"
+        >
+          <input
+            onChange={(e) => setRadio(p.array)}
+            type="radio"
+            name={"priceFilter"}
+            id={`${p._id}`}
+          />
+          <label className="w-full " htmlFor={`${p._id}`}>
+            {p.name}
+          </label>
+        </fieldset>
+      ))}
+    </div>
+  );
+};
+
+export default PriceFilter;
