547c8c3ce1303f8e020167410805b998e01b0fa0
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-11-12 17:32:14 +0500

    background color change for sidebar

--- src/components/Filters/CategoryFilter.js
+++ src/components/Filters/CategoryFilter.js
@@ -11,7 +11,7 @@ const CategoryFilter = ({
       <div
         className={`text-sm sm:text-base ${
           menu ? "flex w-[50vw] visible" : "p-0 w-0 overflow-hidden invisible"
-        } px-1 py-3 min-h-min flex-col items-center border rounded-lg bg-gray-100 transition-[width] my-7 sm:flex sm:w-64 sm:px-4 sm:pb-3 sm:visible `}
+        } px-1 py-3 min-h-min flex-col items-center border-y transition-[width] my-7 sm:flex sm:w-64 sm:px-4 sm:pb-3 sm:visible `}
       >
         <h5 className="self-start mb-3 text-[#000000de] font-semibold">
           By Category
@@ -19,7 +19,7 @@ const CategoryFilter = ({
         {categories?.map((category) => (
           <fieldset
             key={category._id}
-            className="flex gap-1  w-full border-b-2 border-gray-400"
+            className="flex gap-1 w-full  last:border-none  border-b border-[#002147be]"
           >
             <input
               onChange={(e) =>
--- src/components/Filters/PriceFilter.js
+++ src/components/Filters/PriceFilter.js
@@ -6,13 +6,15 @@ const PriceFilter = ({ menu, setRadio }) => {
     <div
       className={`text-sm sm:text-base ${
         menu ? "flex w-[50vw] visible" : "p-0 w-0 overflow-hidden invisible"
-      } px-1 py-3 min-h-min flex-col items-center border rounded-lg bg-gray-100 transition-[width] my-7 sm:flex sm:w-64 sm:px-4 sm:pb-3 sm:visible `}
+      } px-1 py-3 min-h-min flex-col items-center border-y  transition-[width] my-7 sm:flex sm:w-64 sm:px-4 sm:pb-3 sm:visible `}
     >
-      <h5 className="self-start mb-3 text-[#000000de] font-semibold">By Price</h5>
+      <h5 className="self-start mb-3 text-[#000000de] font-semibold">
+        By Price
+      </h5>
       {Prices?.map((p) => (
         <fieldset
           key={p._id}
-          className="flex gap-1  w-full border-b-2 border-gray-400"
+          className="flex gap-1 last:border-none w-full border-b border-[#002147be] "
         >
           <input
             onChange={(e) => setRadio(p.array)}
