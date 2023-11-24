681a41cff28d08d8f6689e3d213bdfbb946b7f85
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-11-24 19:33:15 +0500

    related products added

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { switchAlert, triggerAlert } from "../store/UserSlice";
 import { useDispatch } from "react-redux";
 
@@ -7,6 +7,8 @@ const ProductDetail = () => {
   const dispatch = useDispatch();
   const { slug } = useParams();
   const [product, setProduct] = useState();
+  //Related products
+  const [relatedProduct, setRelatedProduct] = useState([]);
   useEffect(() => {
     const getSingleProduct = async () => {
       try {
@@ -14,7 +16,10 @@ const ProductDetail = () => {
           `${process.env.REACT_APP_API}/api/v1/product/get-product/${slug}`
         );
         const json = await res.json();
-        if (json.success) setProduct(json.product);
+        if (json.success) {
+          setProduct(json.product);
+          getRelatedProducts(json.product._id, json.product.category._id);
+        }
       } catch (error) {
         dispatch(triggerAlert(error));
         //Hiding alert
@@ -23,6 +28,22 @@ const ProductDetail = () => {
         }, 2000);
       }
     };
+
+    //Related products
+    const getRelatedProducts = async (pid, cid) => {
+      try {
+        const res = await fetch(
+          `${process.env.REACT_APP_API}/api/v1/product/related-product/${pid}/${cid}`
+        );
+        const json = await res.json();
+        if (json.success) {
+          setRelatedProduct(json.relatedProduct);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     getSingleProduct();
   }, [dispatch, slug]);
 
@@ -53,20 +74,72 @@ const ProductDetail = () => {
             <div className="mt-3">
               {/* Description */}
               <p className=" pb-3">{product.description}</p>
-                {/* Category of product */}
+              {/* Category of product */}
               <p className="text-sm mt-2">
-                <strong className="text-xs font-medium md:text-sm block opacity-70">Category:</strong>
+                <strong className="text-xs font-medium md:text-sm block opacity-70">
+                  Category:
+                </strong>
                 <strong className="font-semibold">
                   {product.category.name}
                 </strong>
               </p>
             </div>
 
-            <button className="border w-max px-2 py-1 text-[#fffafa] mt-4 bg-blue-500 rounded-md">Add to Cart</button>
+            <button className="border w-max px-2 py-1 text-[#fffafa] mt-4 bg-blue-500 rounded-md">
+              Add to Cart
+            </button>
           </div>
         </div>
       )}
-      <hr className="w-[95%] my-7 m-auto" />
+      <hr className="w-[95%] mt-7 mb-4 m-auto" />
+
+      {/* RELATED PRODUCTS */}
+      <div className="flex justify-center flex-col items-cente gap-4 p-2 mb-10">
+        <div>
+          <h3 className="font-semibold mb-4 opacity-80 text-center">
+            Related Products
+          </h3>
+        </div>
+        <div className="flex gap-3 overflow-x-auto py-2 w-full sm:pl-4 sm:justify-center">
+          {relatedProduct ? (
+            relatedProduct.map((product) => (
+              <Link
+                to={`/product/${product.slug}`}
+                key={product._id}
+                className="shadow-md  pb-1 rounded overflow-hidden min-w-[120px] w-40 sm:w-48 sm:min-h-[12rem] sm:h-[18rem] transition hover:scale-[1.02] focus:scale-[1.02]"
+              >
+                <div className="h-32">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
+                    alt={product.name}
+                  />
+                </div>
+                <div className="flex flex-col gap-1 p-1 h-[calc(100%-8.1rem)]  sm:p-3">
+                  <p className="text-sm text-sky-700 sm:text-base font-semibold my-1 ">
+                    {product.name}
+                  </p>
+                  <div className="flex flex-col justify-between h-full">
+                    <p className="text-xs sm:text-sm ">
+                      {product.description.length > 30
+                        ? product.description.substring(0, 50) + "..."
+                        : product.description}
+                    </p>
+                  </div>
+
+                  <div className=" mt-2">
+                    <p className="text-sm sm:text-base mt-1 text-gray-500 justify-self-end font-mono self-end font-semibold">
+                      {product.price}$
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p>No Product</p>
+          )}
+        </div>
+      </div>
     </>
   );
 };
