4e5b040aca75a2a856488aea06c9fa162078758d
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-12-10 14:23:57 +0500

    UI changes in Product Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -203,26 +203,26 @@ const Home = () => {
             Total Product : {productCount}
           </p>
         </div>
+        {/* Mapping Product's array */}
         <div className="my-4 p-1 min-w-[60vw] sm:pl-4  flex gap-1 flex-wrap sm:gap-2">
           {products?.map((product) => (
-            <Link
-              to={`/product/${product.slug}`}
+            <div
               key={product._id}
-              className="shadow-md border pb-1 rounded overflow-hidden w-40 sm:w-48 sm:min-h-[12rem] sm:h-[18rem] transition hover:scale-[1.02] focus:scale-[1.02]"
+              className="shadow-md border pb-1 rounded overflow-hidden w-44 sm:w-48 sm:min-h-[12rem] sm:h-[20rem] transition hover:scale-[1.02] focus:scale-[1.02]"
             >
-              <div className="h-32">
+              <div className="h-36">
                 <img
                   className="h-full w-full object-cover"
                   src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
                   alt={product.name}
                 />
               </div>
-              <div className="flex flex-col gap-1 p-1 h-[calc(100%-8.1rem)]  sm:p-3">
+              <div className="flex flex-col gap-1 p-1 h-[calc(100%-9.1rem)]  sm:p-3">
                 <p className="text-sm text-sky-700 sm:text-base font-semibold my-1 ">
                   {product.name}
                 </p>
                 <div className="flex flex-col justify-between h-full">
-                  <p className="text-xs sm:text-sm ">
+                  <p className="text-xs sm:text-sm">
                     {product.description.length > 30
                       ? product.description.substring(0, 50) + "..."
                       : product.description}
@@ -230,15 +230,18 @@ const Home = () => {
                 </div>
 
                 <div className="flex justify-between items-center mt-2">
-                  <button className="bg-blue-600 text-white text-sm sm:text-base rounded py-[.2rem] px-1 hover:bg-blue-800">
+                  <Link
+                    to={`/product/${product.slug}`}
+                    className="bg-blue-600 text-white text-sm sm:text-base rounded py-[.2rem] px-1 hover:bg-blue-800"
+                  >
                     More Detail
-                  </button>
+                  </Link>
                   <p className="text-sm sm:text-base mt-1 text-gray-500 justify-self-end font-mono self-end font-semibold">
                     {product.price}$
                   </p>
                 </div>
               </div>
-            </Link>
+            </div>
           ))}
         </div>
 
