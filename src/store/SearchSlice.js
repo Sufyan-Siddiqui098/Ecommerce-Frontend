628bb680050fac9a470cc04f18145823a69849e5
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-12-14 00:55:52 +0500

    better the readability of code

--- src/store/SearchSlice.js
+++ src/store/SearchSlice.js
@@ -1,18 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const searchSlice = createSlice({
-    name: 'searchProduct',
-    initialState: {
-        searchResult: []
-    }, 
-    reducers: {
-        setSearchResult: (state, action)=>{
-            if(action.payload.success){
-                state.searchResult = action.payload.result;
-            }
-        }
-    }
-}) 
+  name: "searchProduct",
+  initialState: {
+    searchResult: [],
+  },
+  reducers: {
+    setSearchResult: (state, action) => {
+      if (action.payload.success) {
+        state.searchResult = action.payload.result;
+      }
+    },
+  },
+});
 
-export const {setSearchResult} = searchSlice.actions;
+export const { setSearchResult } = searchSlice.actions;
 export default searchSlice.reducer;
