bd57019a507736e2c3f6b62380fa42d34e0bd3b5
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-12-14 00:56:33 +0500

    CartReducer added in Redux Store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,11 +1,13 @@
-import {configureStore} from '@reduxjs/toolkit'
-import userReducer from './UserSlice'
-import searchReducer from './SearchSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./UserSlice";
+import searchReducer from "./SearchSlice";
+import cartReducer from "./CartSlice";
 const store = configureStore({
-    reducer:{
-        user: userReducer,
-        searchProduct: searchReducer
-    }
-})
+  reducer: {
+    user: userReducer,
+    searchProduct: searchReducer,
+    cart: cartReducer,
+  },
+});
 
 export default store;
